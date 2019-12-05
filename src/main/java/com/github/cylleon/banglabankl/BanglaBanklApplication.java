18de81285fb0360c17d822198383bc43b9d1f070
Cylleon <cylleondev@gmail.com>
2019-12-05 19:23:25 +0100

    initial commit

---
+++ src/main/java/com/github/cylleon/banglabankl/BanglaBanklApplication.java
@@ -0,0 +1,13 @@
+package com.github.cylleon.banglabankl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanglaBanklApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanglaBanklApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/cylleon/banglabankl/BanglaBanklApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cylleon.banglabankl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanglaBanklApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
